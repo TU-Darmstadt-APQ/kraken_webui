51adda6e6504997e0596ddd5aae152ec6eef836a
GGalya1 <garturmail@gmail.com>
2024-11-21 20:33:58 +0100

    sorting-update
    You can sort by attributes.
    
    Since custom attributes were inserted, the sort method is large (this method contains all possible sorts for every possible type of Post).

--- src/app/components/UI/select/MySelect.tsx
+++ src/app/components/UI/select/MySelect.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { MySelectProps, Post } from "@/app/types";
+import React from 'react';
+import { MySelectProps, Post } from '@/app/types';
+import cl from './MySelect.module.css'
+
 
 /**
  * A reusable and customizable dropdown (select) component.
