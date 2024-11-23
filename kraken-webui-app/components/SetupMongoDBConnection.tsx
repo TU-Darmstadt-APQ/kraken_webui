874dac7e443ce7f1091006254214fd7de664bff5
David Le <david.le03@outlook.de>
2024-11-23 01:45:06 +0100

    fix(database): Add mongodb connection initial setup & MIT license
    Troubleshooting "MongooseError: Operation `testsensors.find()` buffering timed out after 10000ms"

--- kraken-webui-app/components/SetupMongoDBConnection.tsx
+++ kraken-webui-app/components/SetupMongoDBConnection.tsx
@@ -1,38 +1,37 @@
 // getting-started.js
 const mongoose = require('mongoose');
 
-async function StartSetup() {
-  await mongoose.connect('mongodb://admin:pass@127.0.0.1:8090/sensor_config');
-
+function StartSetup() {
+  console.log("START CONNECTION")
+  mongoose.connect('mongodb://admin:pass@127.0.0.1:8090/sensor_config').catch((error : any) => console.log("Catch Error:", error));;
+  mongoose.connection.on('error', (err : any) => console.log("Catch Connection Error:", err))
+  console.log("CONNECTION SETUP DONE")
 }
 
-const SensorSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  }
-});
-
-const Sensor = mongoose.models.TestSensor || mongoose.model('TestSensor', SensorSchema);
-
-// Adding a test sensor
-const testSensor = new Sensor({ name: 'Test1' });
-console.log(testSensor.name); // 'Silence'
-console.log(testSensor)
-// await testSensor.save();
-const sensors = await Sensor.find();
-console.log(sensors);
-const plainSensor = testSensor.toObject();
-
-/**
-type Props = {
-  key: value;
+function WriteTest() {
+  const SensorSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    }
+  });
+  
+  const Sensor = mongoose.models.TestSensor || mongoose.model('TestSensor', SensorSchema);
+  
+  // Adding a test sensor
+  const testSensor = new Sensor({ name: 'Test1' });
+  // await testSensor.save();
+  const sensors = Sensor.find();
+  console.log(sensors);
+  // const plainSensor = testSensor.toObject();
 }
-*/
+
+
 export default function SetupMongoDBConnection() {
   StartSetup()
+  WriteTest()
   return (
-    <span>{JSON.stringify(plainSensor)}</span>
+    <span></span>
   )
 }
    
