4bb5d41922c561d68ce5779667598de74eb176a9
GGalya1 <garturmail@gmail.com>
2024-11-28 20:55:07 +0100

    column_selection-update
    - columns in the table view can be hidden and displayed as required
    - table view is flexible now

--- src/app/components/TableItem.tsx
+++ src/app/components/TableItem.tsx
@@ -10,52 +10,58 @@ import { TableItemProps } from '@/app/types';
  * @param {Post} post - The data object for the table row.
  * @param {(post: Post) => void} props.remove - Callback to handle the removal of a row.
  */
-const TableItem: React.FC<TableItemProps> = ({post, remove, edit}) => {
+const TableItem: React.FC<TableItemProps> = ({post, remove, edit, selectedColumns}) => {
     return(
         <tr>
             {/* Displaying properties of the `post` object */}
-            <td>{post.id}</td>
-            <td>{post.title}</td>
-            <td>{post.description}</td>
-            <td>{post.date_created
+            {selectedColumns.id && <td>{post.id}</td>}
+            {selectedColumns.title && <td>{post.title}</td>}
+            {selectedColumns.description && <td>{post.description}</td>}
+
+            {selectedColumns.date_created && <td>{post.date_created
                 ? `${post.date_created.day}.${post.date_created.month}.${post.date_created.year}`
                 : "Date not given"}
-            </td>
-            <td>{post.date_modified
+            </td>}
+
+            {selectedColumns.date_modified && <td>{post.date_modified
                 ? `${post.date_modified.day}.${post.date_modified.month}.${post.date_modified.year}`
                 : "Date not given"}
-            </td>
-            <td>
+            </td>}
+
+            {selectedColumns.enabled && <td>
                 {post.enabled == true ? (
                 <div style={{ width: "10px", height: "10px", backgroundColor: "green", borderRadius: "50%" }}></div>
             ): post.enabled == false ? (
                 <div style={{ width: "10px", height: "10px", backgroundColor: "red", borderRadius: "50%" }}></div>
             ) : null
                 }
-            </td>
-            <td>{post.label}</td>
-            <td>{post.uuid}</td>
-            <td>
-  {post.config && Object.entries(post.config).length > 0 ? (
-    <div>
-      <p>config {"{"}</p>
-      <ul style={{ paddingLeft: "20px", margin: "5px 0" }}>
-        {Object.entries(post.config).map(([key, value]) => (
-          <li key={key} style={{ listStyleType: "none" }}>
-            - {key}: {JSON.stringify(value)}
-          </li>
-        ))}
-      </ul>
-      <p>{"}"}</p>
-    </div>
-  ) : (
-    "No configuration given"
-  )}
-</td>
-            <td>{post.on_connect}</td>
+            </td>}
+
+            {selectedColumns.label && <td>{post.label}</td>}
+            {selectedColumns.uuid && <td>{post.uuid}</td>}
+            {selectedColumns.config && 
+              <td>
+                {post.config && Object.entries(post.config).length > 0 ? (
+                  <div>
+                    <p>config {"{"}</p>
+                    <ul style={{ paddingLeft: "20px", margin: "5px 0" }}>
+                      {Object.entries(post.config).map(([key, value]) => (
+                        <li key={key} style={{ listStyleType: "none" }}>
+                          - {key}: {JSON.stringify(value)}
+                        </li>
+                      ))}
+                    </ul>
+                    <p>{"}"}</p>
+                  </div>
+                  ) : (
+                    "No configuration given"
+                  )}
+              </td>}
+
+            {selectedColumns.on_connect && <td>{post.on_connect}</td>}
             
-            {/* Edit button */}
-            <td style={{ borderBottomStyle: 'hidden', borderTopStyle: 'hidden', borderRightStyle: 'hidden' }}>
+            {/* Edit button and delete button with callback */}
+            <td style={{ borderBottomStyle: 'hidden', borderTopStyle: 'hidden', borderRightStyle: 'hidden'}}>
               <MyButton className="list-button" onClick={() => edit(post)}>
                 <img 
                   src="/edit.png" 
@@ -64,10 +70,8 @@ const TableItem: React.FC<TableItemProps> = ({post, remove, edit}) => {
                   width={20} 
                   height={20} 
                 />
-              </MyButton></td>
-            
-            {/* Delete button with callback */}
-            <td style={{ borderBottomStyle: 'hidden', borderTopStyle: 'hidden', borderRightStyle: 'hidden' }}>
+              </MyButton>
+
               <MyButton onClick={() => remove(post)} className="list-button">
                 <img 
                   src="/trashCan.png" 
@@ -76,7 +80,8 @@ const TableItem: React.FC<TableItemProps> = ({post, remove, edit}) => {
                   width={20} 
                   height={20} 
                 />
-              </MyButton></td>
+              </MyButton>
+            </td>
         </tr>
     );
 };
