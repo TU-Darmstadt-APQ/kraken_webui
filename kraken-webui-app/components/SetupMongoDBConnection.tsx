1014eb117c087b6799df928292e750dd328d6b52
David Le <david.le03@outlook.de>
2024-11-29 11:27:57 +0100

    fix(database): changed ip in connection string to fix connection closing

--- kraken-webui-app/components/SetupMongoDBConnection.tsx
+++ kraken-webui-app/components/SetupMongoDBConnection.tsx
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 function StartSetup() {
   console.log("START CONNECTION")
-  mongoose.connect(`mongodb://${config.krakenConfigsMongodbCredentials}@${config.krakenConfigsMongodbHost}:${config.krakenConfigsMongodbPort}/sensor_config`).catch((error : any) => console.log("Catch Error:", error));
+  mongoose.connect(`mongodb://${config.krakenConfigsMongodbCredentials}@${config.krakenConfigsMongodbHost}:${config.krakenConfigsMongodbPort}/sensor_config?authSource=admin`).catch((error : any) => console.log("Catch Error:", error));
   mongoose.connection.on('error', (err : any) => console.log("Catch Connection Error:", err))
   console.log("CONNECTION SETUP DONE")
 }
@@ -29,16 +29,16 @@ function WriteTest() {
   // const plainSensor = testSensor.toObject();
 }
 
-async function run() {
+function run(): any {
   try {
     console.log("Starting Connection Test...")
-    await mongoose.connection.db.admin().command({ ping: 1 });
+    mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
     console.log("Connection Test through.")
   } finally {
     // Ensures that the client will close when you finish/error
     console.log("Disconnecting from MongoDB!")
-    await mongoose.disconnect();
+    mongoose.disconnect();
     console.log("Disconnected");
   }
 }
