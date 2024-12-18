7c4ca8ab733400a3f2f79c755c57790f7e8d4502
GGalya1 <garturmail@gmail.com>
2024-12-18 16:12:26 +0100

    - Added Zod library for input field validation - Implemented validation for the search field - Added custom color for selected text using the ::selection CSS pseudo-element

--- src/app/components/UI/input/MyInput.tsx
+++ src/app/components/UI/input/MyInput.tsx
@@ -1,5 +1,6 @@
 import React, { ForwardedRef, InputHTMLAttributes } from "react";
 import classes from './MyInput.module.css';
+import tooltipStyles from './../tooltip/MyTooltip.module.css';
 
 
 /**
@@ -13,11 +14,24 @@ import classes from './MyInput.module.css';
  * @param {ForwardedRef<HTMLInputElement>} ref - React ref for accessing the underlying input element.
  * @returns {JSX.Element} A styled `<input>` element.
  */
-type MyInputProps = InputHTMLAttributes<HTMLInputElement>;
+interface MyInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: string; // Optional Text for Error Tooltip
+  tooltipPosition?: 'top' | 'bottom' | 'left' | 'right'; // Optional Field for Tooltip orientation
+}
 
-const MyInput = React.forwardRef((props: MyInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+const MyInput = React.forwardRef(({ error, tooltipPosition = 'bottom', ...props }: MyInputProps, ref: ForwardedRef<HTMLInputElement>) => {
     return (
-      <input className={classes.myInput} {...props}/>
+      <div style={{ position: 'relative' }}>
+        <input className={classes.myInput} ref={ref} {...props} />
+        
+        {/* Toolttp for error */}
+        {error && (
+          <div className={`${tooltipStyles.tooltip} ${tooltipStyles[tooltipPosition]}`}>
+            {error}
+            <div className={`${tooltipStyles.arrow} ${tooltipStyles[`arrow-${tooltipPosition}`]}`} />
+          </div>
+        )}
+      </div>
     );
   });
 
