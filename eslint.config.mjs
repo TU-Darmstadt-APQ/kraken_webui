613b72caf28ebd2f80a9f228fa02fb29a30e949e
Tina_Ev_7 <73749470+Christina-Ev@users.noreply.github.com>
2025-01-23 23:27:16 +0100

    added .mjs in the lint-staged config of package.json and formatted eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,42 +10,42 @@ import eslintConfigPrettier from "eslint-config-prettier";
  * @type {import('eslint').Linter.Config[]}
  */
 export default [
-   {
-      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-      languageOptions: {
-         parser: tsParser, // Use TypeScript parser for .ts, .tsx files
-         parserOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-         },
-         globals: {
-            ...globals.browser,
-            ...globals.node,
-         },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser, // Use TypeScript parser for .ts, .tsx files
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
 
-      plugins: {
-         "@typescript-eslint": tseslint, // Changed to object format
-         react: pluginReact, // Changed to object format
-         "@next/next": pluginNext,
-      },
+    plugins: {
+      "@typescript-eslint": tseslint, // Changed to object format
+      react: pluginReact, // Changed to object format
+      "@next/next": pluginNext,
+    },
 
-      ...pluginJs.configs.recommended[0],
-      ...tseslint.configs.recommended[0],
-      ...pluginReact.configs.flat.recommended[0],
+    ...pluginJs.configs.recommended[0],
+    ...tseslint.configs.recommended[0],
+    ...pluginReact.configs.flat.recommended[0],
 
-      settings: {
-         react: {
-            version: "detect", // Automatically detects the version of React
-         },
+    settings: {
+      react: {
+        version: "detect", // Automatically detects the version of React
       },
-      rules: {
-         "no-unused-vars": ["error", { args: "none" }], // Option to allow unused function arguments
-         "no-console": "warn", // Optional, for preventing console.log in production code
-         "eol-last": ["error", "always"], // Ensure files end with a newline
-         "no-useless-escape": "error", // Prevent unnecessary escapes
-      },
-   },
-   // Add prettier config last to ensure it overrides other settings
-   eslintConfigPrettier,
+    },
+    rules: {
+      "no-unused-vars": ["error", { args: "none" }], // Option to allow unused function arguments
+      "no-console": "warn", // Optional, for preventing console.log in production code
+      "eol-last": ["error", "always"], // Ensure files end with a newline
+      "no-useless-escape": "error", // Prevent unnecessary escapes
+    },
+  },
+  // Add prettier config last to ensure it overrides other settings
+  eslintConfigPrettier,
 ];
