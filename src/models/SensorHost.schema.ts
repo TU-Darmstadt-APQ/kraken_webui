976c29769791657ea8b02da35380f84222dbad3d
anshulchahar <rohitchahar0987@gmail.com>
2025-02-02 13:29:27 +0100

    fix: fixed sensorHost schema

--- src/models/SensorHost.schema.ts
+++ src/models/SensorHost.schema.ts
@@ -1,30 +1,19 @@
+import { UUID } from "bson";
 import { z } from "zod";
 
 // Defines the schema for a sensor host as used by the Mongo database
 export const sensorHostEntitySchema = z.object({
-  _id: z.object({
-    $uuid: z.string().uuid(),
-  }),
+  _id: z.instanceof(UUID),
   hostname: z.string(),
   port: z.number().int().nonnegative(),
   pad: z.union([z.number().int().nonnegative(), z.null()]),
   sad: z.union([z.number().int().nonnegative(), z.null()]),
   driver: z.string(),
-  node_id: z.object({
-    $uuid: z.string().uuid(),
-  }),
+  node_id: z.instanceof(UUID),
   reconnect_interval: z.union([z.number().int().nonnegative(), z.null()]),
-  revision_id: z.optional(
-    z.object({
-      $uuid: z.string().uuid(),
-    }),
-  ),
-  date_created: z.object({
-    $date: z.string().datetime(),
-  }),
-  date_modified: z.object({
-    $date: z.string().datetime(),
-  }),
+  revision_id: z.optional(z.instanceof(UUID)),
+  date_created: z.instanceof(Date),
+  date_modified: z.instanceof(Date),
   enabled: z.boolean(),
   label: z.optional(z.union([z.string(), z.null()])),
   description: z.union([z.string(), z.null()]),
@@ -43,6 +32,7 @@ export const sensorHostDTOSchema = z.object({
   driver: z.string(),
   node_id: z.string().uuid(),
   reconnect_interval: z.union([z.number().int().nonnegative(), z.null()]),
+  revision_id: z.optional(z.string().uuid()),
   date_created: z.string().datetime(),
   date_modified: z.string().datetime(),
   enabled: z.boolean(),
@@ -55,21 +45,21 @@ export type sensorHostDTO = z.infer<typeof sensorHostDTOSchema>;
 export const sensorHostDTO = {
   convertFromEntity(entity: sensorHostEntity): sensorHostDTO {
     const candidate: sensorHostDTO = {
-      id: entity._id.$uuid,
+      id: entity._id.toString(),
       hostname: entity.hostname,
       port: entity.port,
       pad: entity.pad,
       sad: entity.sad,
       driver: entity.driver,
-      node_id: entity.node_id.$uuid,
+      node_id: entity.node_id.toString(),
       reconnect_interval: entity.reconnect_interval,
-      date_created: entity.date_created.$date,
-      date_modified: entity.date_modified.$date,
+      revision_id: entity.revision_id?.toString() || undefined,
+      date_created: entity.date_created.toISOString(),
+      date_modified: entity.date_modified.toISOString(),
       enabled: entity.enabled,
       label: entity.label,
       description: entity.description,
     };
-
     return sensorHostDTOSchema.parse(candidate);
   },
 };
