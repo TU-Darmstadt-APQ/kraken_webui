ea400859eacecbfd0227c72705276483b458c5a3
Tina_Ev_7 <73749470+Christina-Ev@users.noreply.github.com>
2025-02-03 18:24:36 +0100

    Add Server Action for deleting Sensors

---
+++ src/actions/action_deleteSensors.ts
@@ -0,0 +1,18 @@
+"use server";
+import { deleteSensor } from "@/components/mongodb/DBConnector";
+export async function deleteSensorAction(uuid: string) {
+  try {
+    const message = await deleteSensor(uuid);
+    return { success: true, message }; // Return success response
+  } catch (error: unknown) {
+    // Handle errors and provide a friendly message
+    console.error("Error deleting sensor:", error);
+    if (error instanceof Error) {
+      return { success: false, message: error.message };
+    }
+    return {
+      success: false,
+      message: "An unknown error occurred while deleting the sensor",
+    };
+  }
+}
