253a71c1dff2c13b30b63eaaef9fdab704703836
GGalya1 <garturmail@gmail.com>
2025-02-20 10:58:45 +0100

    Hotfix: Comment added to the InputRow.tsx component and typos corrected

--- src/components/UI/InputRow.tsx
+++ src/components/UI/InputRow.tsx
@@ -11,6 +11,35 @@ import PostForm from "../PostForm";
 import styles from "@/styles/TableItem.module.css";
 import { v4 as uuidv4 } from "uuid";
 
+/**
+ * InputRow component renders a row in the UI that allows a user to either create a new post or edit an existing one.
+ *
+ * This component displays various input fields such as UUID, label, topic, driver, and others based on the
+ * provided selectedColumns prop.
+ * A modal window is used to modify the configuration through a PostForm.
+ *
+ * @component
+ * @param {InputRowProps} props - The props for the InputRow component.
+ * @param {boolean} props.visible - Determines whether the input row is shown.
+ * @param {function(boolean): void} props.setVisible - Callback to change the visibility of the input row.
+ * @param {function(Post): void} props.createPost - Callback to create a new post.
+ * @param {function(Post): void} props.edit - Callback to edit an existing post.
+ * @param {Post} [props.postToEdit] - Optional post to pre-fill the input fields for editing.
+ * @param {Object} props.selectedColumns - An object indicating which columns/fields should be displayed.
+ *
+ * @example
+ * const selectedColumns = {
+ *   uuid: true,
+ *   label: true,
+ *   enabled: true,
+ *   topic: true,
+ *   driver: true,
+ *   config: true,
+ *   on_connect: true,
+ * };
+ *
+ * />
+ */
 const InputRow: React.FC<InputRowProps> = ({
   visible,
   setVisible,
@@ -123,7 +152,7 @@ const InputRow: React.FC<InputRowProps> = ({
             value={post.topic}
             onChange={(e) => setPost({ ...post, topic: e.target.value })}
             type="text"
-            placeholder="Label"
+            placeholder="Topic"
           />
         </div>
       )}
@@ -133,7 +162,7 @@ const InputRow: React.FC<InputRowProps> = ({
             value={post.driver}
             onChange={(e) => setPost({ ...post, driver: e.target.value })}
             type="text"
-            placeholder="Label"
+            placeholder="Driver"
           />
         </div>
       )}
